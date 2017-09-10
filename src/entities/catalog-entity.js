8ac5e5574612e91d35bbab32e7c327fda3f3a553
forafish <fora.luo@gmail.com>
2017-09-11 02:44:25 +0800

    remove catalog service (replaced by user-collections and user-favorites services)

--- src/entities/catalog-entity.js
+++
@@ -1,7 +0,0 @@
-import Entity from 'mostly-entity';
-
-const CatalogEntity = new Entity('Catalog');
-
-CatalogEntity.excepts('createdAt', 'updatedAt', 'destroyedAt');
-
-export default CatalogEntity.asImmutable();
