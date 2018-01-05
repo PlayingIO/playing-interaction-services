2d8546780efa47a4e33fe60d705fa5a0b3a95678
forafish <fora.luo@gmail.com>
2018-01-05 18:54:00 +0800

    add Tagable facet

--- src/constants/doc-types.js
+++ src/constants/doc-types.js
@@ -7,6 +7,7 @@ export default Object.assign({
     "facets":[
       "Versionable",
       "Collection",
+      "Tagable",
       "NotCollectionMember"
     ]
   },
@@ -16,6 +17,7 @@ export default Object.assign({
     "facets":[
       "Versionable",
       "Collection",
+      "Tagable",
       "NotCollectionMember"
     ]
   },
