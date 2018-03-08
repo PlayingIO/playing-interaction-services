a3f31729d64db82907723065a0695a501dd7be5a
forafish <fora.luo@gmail.com>
2018-03-08 11:34:33 +0800

    move user-permission service to playing-user-services

--- src/entities/user-permission-entity.js
+++
@@ -1,7 +0,0 @@
-import Entity from 'mostly-entity';
-
-const UserPermissionEntity = new Entity('UserPermission');
-
-UserPermissionEntity.excepts('createdAt', 'updatedAt', 'destroyedAt');
-
-export default UserPermissionEntity.asImmutable();
