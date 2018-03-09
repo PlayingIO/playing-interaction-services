b2295561e5313f57e4a359abfa20c89c565aa8f5
forafish <fora.luo@gmail.com>
2018-03-09 19:02:37 +0800

    create user feedback instead of upsert

--- src/services/user-feedback/user-feedback-service.js
+++ src/services/user-feedback/user-feedback-service.js
@@ -56,11 +56,8 @@ class UserFeedbackService extends Service {
     return getSubjects.then((docs) => {
       if (!docs || docs.length !== ids.length) throw new Error('some data.subject(s) not exists');
       return Promise.all(docs.map((doc) => {
-        return super._upsert(null, data, {
-          subject: doc.id,
-          type: doc.type,
-          user: data.user
-        });
+        const feedback = fp.merge({ subject: doc.id, type: doc.type }, data);
+        return super.create(feedback);
       }));
     });
   }
