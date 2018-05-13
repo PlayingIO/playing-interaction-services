cc460e52918066c906900c7ab03c83931d916890
forafish <fora.luo@gmail.com>
2018-05-13 19:11:50 +0800

    change document to subject of user-favorite model

--- src/models/user-favorite.model.js
+++ src/models/user-favorite.model.js
@@ -9,7 +9,7 @@ const options = {
  */
 const fields = {
   favorite: { type: 'ObjectId', required: true }, // favorite id
-  document: { type: 'ObjectId', required: true }, // document id
+  subject: { type: String, required: true },      // subject or document id
   payload: { type: 'Mixed' },                     // extra info
   type: { type: String, required: true },         // document type
   user: { type: 'ObjectId', required: true }      // user id
@@ -18,8 +18,7 @@ const fields = {
 export default function model (app, name) {
   const mongoose = app.get('mongoose');
   const schema = new mongoose.Schema(fields, options);
-  schema.plugin(plugins.sortable, { classify: 'favorite' });
-  schema.index({ favorite: 1, document: 1, user: 1 }, { unique: true });
+  schema.index({ favorite: 1, subject: 1, user: 1 }, { unique: true });
   return mongoose.model(name, schema);
 }
 
