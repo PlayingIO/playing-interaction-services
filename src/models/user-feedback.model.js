24652d88d20ce54cc80db404c60e1b7b3d4f7dd0
forafish <fora.luo@gmail.com>
2018-05-13 21:32:08 +0800

    update user-feedback model

--- src/models/user-feedback.model.js
+++ src/models/user-feedback.model.js
@@ -6,19 +6,19 @@ const options = {
  * feedback of a document
  */
 const fields = {
+  subject: { type: String },                    // subject or document
+  payload: { type: 'Mixed' },                   // extra info
+  type: { type: String },                       // subject document type
+  user: { type: 'ObjectId', required: true },   // share with user
   comment: { type: String },                    // comment
   rating: { type: Number },                     // rating
-  tag: { type: String },                        // tag
-  payload: { type: 'Mixed' },                   // extra info
-  subject: { type: String },                    // subject or document
-  type: { type: String },                       // subject document type
-  user: { type: 'ObjectId', required: true }    // share with user
+  tag: { type: String }                         // tag
 };
 
 export default function model (app, name) {
   const mongoose = app.get('mongoose');
   const schema = new mongoose.Schema(fields, options);
-  schema.index({ subject: 1, user: 1 });
+  schema.index({ subject: 1, type: 1, user: 1 });
   return mongoose.model(name, schema);
 }
 
