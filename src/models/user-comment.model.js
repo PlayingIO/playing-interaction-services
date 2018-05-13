b9330dc73ec0eb8f907406be32514047a0ec520d
forafish <fora.luo@gmail.com>
2018-05-13 22:00:38 +0800

    update user-comment model

--- src/models/user-comment.model.js
+++ src/models/user-comment.model.js
@@ -6,22 +6,22 @@ const options = {
  * Comments of a document
  */
 const fields = {
+  subject: { type: String, required: true },      // subject or document id
+  payload: { type: 'Mixed' },                     // extra info
+  type: { type: String, required: true },         // subject document type
+  user: { type: 'ObjectId', required: true },     // commenter id
   audiences: [{ type: String }],                  // public/private or somebody
+  collpased: { type: Boolean, default: false },   // is collpased
   archived: { type: Boolean, default: false },    // is archived
   comment: { type: String, required: true },      // comment
-  commentedAt: { type: Date },                    // commented at
-  likesCount: { type: Number, default: 0 },       // number of likes
-  mimetype: { type: String },                     // mimetype of content
-  payload: { type: 'Mixed' },                     // extra info
-  subject: { type: String, required: true },      // subject or document id
-  type: { type: String, required: true },         // subject document type
-  user: { type: 'ObjectId', required: true }      // commenter id
+  commentedAt: { type: Date }                     // commented at
 };
 
 export default function model (app, name) {
   const mongoose = app.get('mongoose');
   const schema = new mongoose.Schema(fields, options);
-  schema.index({ subject: 1, user: 1 });
+  schema.index({ subject: 1, type: 1, user: 1 });
+  schema.index({ commentedAt: 1, user: 1 });
   return mongoose.model(name, schema);
 }
 
