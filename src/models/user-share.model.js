a04edc64d6bc7a0ef683ac451451448cc3c1ad13
forafish <fora.luo@gmail.com>
2018-05-13 20:53:12 +0800

    add hashid for sharing

--- src/models/user-share.model.js
+++ src/models/user-share.model.js
@@ -6,6 +6,7 @@ const options = {
  * Share of a document
  */
 const fields = {
+  hashid: { type: String, required: true },       // unique hash id for sharing
   subject: { type: String, required: true },      // subject or document
   payload: { type: 'Mixed' },                     // extra info
   type: { type: String, required: true },         // subject document type
@@ -16,6 +17,7 @@ const fields = {
 export default function model (app, name) {
   const mongoose = app.get('mongoose');
   const schema = new mongoose.Schema(fields, options);
+  schema.index({ hashid: 1 }, { unique: true });
   schema.index({ subject: 1, group: 1, user: 1 }, { unique: true });
   return mongoose.model(name, schema);
 }
