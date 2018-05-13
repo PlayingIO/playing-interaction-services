e3e6b77a60d4e7ae9b6b9460b098d45ee1f45bf4
forafish <fora.luo@gmail.com>
2018-05-13 19:52:54 +0800

    getSubjects by type

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,9 @@
 import assert from 'assert';
 import { helpers } from 'mostly-feathers-mongoose';
+import { plural } from 'pluralize';
 
-export const getSubjects = async (app, ids, params) => {
-  const svcDocuments = app.service('documents');
+export const getSubjects = async (app, type, ids, params) => {
+  const svcDocuments = app.service(plural(type));
   const subjects = await svcDocuments.find({
     query: { _id: { $in: ids }, $select: ['type'] },
     user: params.user,
