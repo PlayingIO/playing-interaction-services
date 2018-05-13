ef9e82b959d670447814c8ec7c404d747869e6d7
forafish <fora.luo@gmail.com>
2018-05-13 20:01:29 +0800

    change share with group to string id

--- src/models/user-share.model.js
+++ src/models/user-share.model.js
@@ -6,17 +6,17 @@ const options = {
  * Share of a document
  */
 const fields = {
-  group: { type: 'ObjectId' },                    // share with group
-  payload: { type: 'Mixed' },                     // extra info
   subject: { type: String, required: true },      // subject or document
+  payload: { type: 'Mixed' },                     // extra info
   type: { type: String, required: true },         // subject document type
-  user: { type: 'ObjectId', required: true }      // share with user
+  user: { type: 'ObjectId', required: true },     // share with user
+  group: { type: String }                         // share with group
 };
 
 export default function model (app, name) {
   const mongoose = app.get('mongoose');
   const schema = new mongoose.Schema(fields, options);
-  schema.index({ subject: 1, user: 1 });
+  schema.index({ subject: 1, group: 1, user: 1 }, { unique: true });
   return mongoose.model(name, schema);
 }
 
