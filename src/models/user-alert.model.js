17e68c306233c6cfd89edfa16f1d37e1bfce67f2
forafish <fora.luo@gmail.com>
2018-05-20 23:00:09 +0800

    add user-alert model

---
+++ src/models/user-alert.model.js
@@ -0,0 +1,21 @@
+const options = {
+  timestamps: true
+};
+
+/**
+ * Last read time of each kind of alerts of a user
+ */
+const fields = {
+  alerts: { type: 'Mixed' },                  // last read time of each kind of alerts
+                                              // e.g. { <alertId>: new Date() }
+  user: { type: 'ObjectId', required: true }  // share with user
+};
+
+export default function model (app, name) {
+  const mongoose = app.get('mongoose');
+  const schema = new mongoose.Schema(fields, options);
+  schema.index({ user: 1 });
+  return mongoose.model(name, schema);
+}
+
+model.schema = fields;
